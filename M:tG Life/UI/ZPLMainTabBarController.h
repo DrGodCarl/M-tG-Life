306a5b8be053e7b2979b2ad7a0a75224ef9cebd8
Nick Wallin <nick.wallin@sebrands.com>
2015-12-29 20:52:58 -0600

    initial UI initial commit -- simple storyboard, tab bar controller subclass

---
+++ M:tG Life/UI/ZPLMainTabBarController.h
@@ -0,0 +1,14 @@
+//
+//  ZPLMainTabBarController.h
+//  M:tG Life
+//
+//  Created by Nick Wallin on 12/29/15.
+//  Copyright © 2015 Zero Point Logic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ZPLCoreDataManager.h"
+
+@interface ZPLMainTabBarController : UITabBarController <ZPLCoreDataConsumer>
+
+@end
---
+++ M:tG Life/UI/ZPLMainTabBarController.m
@@ -0,0 +1,39 @@
+//
+//  ZPLMainTabBarController.m
+//  M:tG Life
+//
+//  Created by Nick Wallin on 12/29/15.
+//  Copyright © 2015 Zero Point Logic. All rights reserved.
+//
+
+#import "ZPLMainTabBarController.h"
+
+@interface ZPLMainTabBarController ()
+
+@property (nonatomic, weak) ZPLCoreDataManager* coreDataManager;
+
+@end
+
+@implementation ZPLMainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
